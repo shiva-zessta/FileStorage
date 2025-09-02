638199b127ce0b7003b7a4f9ef6d741920de0ec4
shiva <90833050+shiva-zessta@users.noreply.github.com>
2025-09-02 18:27:43 +0530

    add (#2)
    Co-authored-by: shivaburugula <shivakumar.b@truera.com>

--- obj/Debug/net9.0/FileStorage.AssemblyInfo.cs
+++ obj/Debug/net9.0/FileStorage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def28e5331324e136bcf66818e68de914ab25bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f0b22555d0ac6f43c7ba62ac3698002aa7d426")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
