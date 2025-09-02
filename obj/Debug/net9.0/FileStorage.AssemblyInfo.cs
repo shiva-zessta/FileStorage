1d2fa3179e2ef4e55e7eb3cc76307d4dea6e736e
shivaburugula <shivakumar.b@truera.com>
2025-09-02 17:18:16 +0530

    added swagger intergration

--- obj/Debug/net9.0/FileStorage.AssemblyInfo.cs
+++ obj/Debug/net9.0/FileStorage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def28e5331324e136bcf66818e68de914ab25bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
